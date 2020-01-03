aa560ae094903a34068fc12cd364ea90a77839ff
WalterGaldamezWeb <walter.galdamez.06@gmail.com>
2020-01-03 11:11:47 -0600

    Comparar String

---
+++ src/compararstring/CompararString.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package compararstring;
+
+/**
+ *
+ * @author Walter Galdamez
+ */
+public class CompararString {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String msj="hola";
+    
+        if(msj.equals("hola")){
+            System.out.println("Verdadero");
+        }else{
+            System.out.println("Falso");
+        }
+    }
+    
+}
